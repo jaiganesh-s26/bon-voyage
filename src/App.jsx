3a59eb975a4b80a380195e724262f837f0c058b4
jaiganesh-s26 <jai.ganesh@geekyants.com>
2026-09-25 09:46:22 +0530

    Side bar settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import TopBar from './components/layout/TopBar.jsx'
 import BottomNav from './components/layout/BottomNav.jsx'
+import Sidebar from './components/layout/Sidebar.jsx'
 import Toast from './components/common/Toast.jsx'
 import AnimatedBackground from './components/common/AnimatedBackground.jsx'
 import ProtectedRoute from './components/auth/ProtectedRoute.jsx'
@@ -23,27 +24,31 @@ function App() {
   const showAnimatedBg = ANIMATED_BG_ROUTES.includes(location.pathname)
 
   return (
-    <div className="device-frame">
-      {showChrome && <TopBar />}
+    <div className="app-shell">
+      {showChrome && <Sidebar />}
 
-      <div className="screens-viewport">
-        {showAnimatedBg && <AnimatedBackground />}
+      <div className="device-frame">
+        {showChrome && <TopBar />}
 
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+        <div className="screens-viewport">
+          {showAnimatedBg && <AnimatedBackground />}
 
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/trips" element={<ProtectedRoute><MyTrips /></ProtectedRoute>} />
-          <Route path="/explore" element={<ProtectedRoute><Explore /></ProtectedRoute>} />
-          <Route path="/saved" element={<ProtectedRoute><Saved /></ProtectedRoute>} />
-          <Route path="/itinerary/:id" element={<ProtectedRoute><Itinerary /></ProtectedRoute>} />
-          <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
-        </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/trips" element={<ProtectedRoute><MyTrips /></ProtectedRoute>} />
+            <Route path="/explore" element={<ProtectedRoute><Explore /></ProtectedRoute>} />
+            <Route path="/saved" element={<ProtectedRoute><Saved /></ProtectedRoute>} />
+            <Route path="/itinerary/:id" element={<ProtectedRoute><Itinerary /></ProtectedRoute>} />
+            <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+          </Routes>
+        </div>
+
+        {showChrome && <BottomNav />}
+        <Toast />
       </div>
-
-      {showChrome && <BottomNav />}
-      <Toast />
     </div>
   )
 }
